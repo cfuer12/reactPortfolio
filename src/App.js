e591d7e70e56d5f1421ffa212131af4205078412
Carl <cfuer12@yahoo.com>
2021-04-05 15:36:22 -0700

    added footer js file

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
-        </Project>
-        <Footer /> */}
+        </Project> */}
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer text-center container-fluid py-4 font-small text-light">
+            <span>© 2021 Copyright Carlos Fuerte</span>
+        </footer>
+    );
+}
+
+export default Footer;
